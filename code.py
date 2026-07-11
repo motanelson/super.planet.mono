9c36d974f6c2bef0325d4e8f253798fadaec610f
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-07-11 21:07:33 +0100

    Add files via upload

--- code.py
+++ code.py
@@ -1,12 +1,10 @@
 print("\033c\033[47;31m\ngive me a text .txt file to encript ? ")
 a=input().strip()
 b=a.replace(".txt","")
-f1=open(a,"r")
+f1=open(a,"r",encoding="utf-8")
 f=f1.read()
 f1.close()
-r=f.encode("utf-16")
-counter=0
 
-f1=open(b+".bin","bw")
-f1.write(r)
+f1=open(b+".bin","w",encoding="utf-16")
+f1.write(f)
 f1.close()
